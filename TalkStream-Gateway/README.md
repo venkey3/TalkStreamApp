38137349782603e5ef6b50da948625b4d23c15c6
venkey3 <101055905+venkey3@users.noreply.github.com>
2025-09-08 01:39:07 +0530

    Add files via upload
    Eureka Discovery Server Commit

---
+++ TalkStream-Gateway/README.md
@@ -0,0 +1,1 @@
+# TalkStream-Gateway
---
+++ TalkStream-Gateway/src/test/java/com/api_gateway/TalkStream/TalkStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_gateway.TalkStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
